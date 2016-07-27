8408c0aecfbb6ad6c14109c793a320bf9db389b2
syawlaus <syawlaus@gmail.com>
2016-07-27 17:59:04 +0800

    leetcode

---
+++ 345. Reverse Vowels of a String/main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    string reverseVowels(string s) {
+        int len = s.length();
+        if (len <= 1) {
+            return s;
+        }
+
+        char* pHead = &s[0];
+        char* pTail = &s[len - 1];
+
+        while (true) {
+            findNextVowelForward(pHead);
+            findNextVowelBackward(pTail);
+            if (pHead < pTail) {
+                swapVowel(pHead, pTail);
+                pHead++;
+                pTail--;
+            }
+            else {
+                break;
+            }
+        }
+        return s;
+    }
+
+private:
+    void findNextVowelForward(char*& pHead) {
+        while (true) {
+            if (isVowel(*pHead)) {
+                return;
+            }
+            pHead++;
+        }
+    }
+
+    void findNextVowelBackward(char*& pTail) {
+        while (true) {
+            if (isVowel(*pTail)) {
+                return;
+            }
+            pTail--;
+        }
+    }
+
+    bool isVowel(char c) {
+        return (c == 'a' || c == 'A' ||
+                c == 'e' || c == 'E' ||
+                c == 'i' || c == 'I' ||
+                c == 'o' || c == 'O' ||
+                c == 'u' || c == 'U');
+    }
+
+    void swapVowel(char*& pHead,
+                   char*& pTail) {
+        char temp = *pHead;
+        *pHead = *pTail;
+        *pTail = temp;
+    }
+
+};
+
+int main() {
+    Solution sol;
+    cout << sol.reverseVowels("") << endl;
+    cout << sol.reverseVowels("hello") << endl;
+    cout << sol.reverseVowels("leetcode") << endl;
+}
