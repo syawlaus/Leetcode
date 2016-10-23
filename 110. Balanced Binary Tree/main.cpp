75949c5d74ecce94a9c5767d225029997a3e99a1
syawlaus <syawlaus@gmail.com>
2016-10-23 17:04:45 +0800

    Leetcode 110

---
+++ 110. Balanced Binary Tree/main.cpp
@@ -0,0 +1,57 @@
+#include <frequently-used-code-snippets.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isBalanced(TreeNode *root) {
+        if (root == NULL) {
+            return true;
+        }
+
+        // 判断左子树是否平衡
+        bool isLeftBalanced = isBalanced(root->left);
+        if (! isLeftBalanced) {
+            return false;
+        }
+
+        // 判断右子树是否平衡
+        bool isRightBalanced = isBalanced(root->right);
+        if (! isRightBalanced) {
+            return false;
+        }
+
+        // 左、右子树都平衡，判断两者高度差是否不大于 1
+        int leftHeight = getHeight(root->left);
+        int rightHeight = getHeight(root->right);
+
+        return (abs(leftHeight - rightHeight) <= 1);
+    }
+
+private:
+    int getHeight(TreeNode *root) {
+        if (root == NULL) {
+            return 0;
+        }
+
+        int leftHeight = getHeight(root->left);
+        int rightHeight = getHeight(root->right);
+        
+        return MAX_TWO(leftHeight, rightHeight) + 1;
+    }
+};
+
+int main() {
+    TreeNode A(1);
+    TreeNode B(2);
+    TreeNode C(3);
+    TreeNode D(4);
+    TreeNode E(5);
+
+    A.left = &B;
+    A.right = &C;
+    B.left = &D;
+    D.left = &E;
+
+    Solution sol;
+    cout << sol.isBalanced(&A);
+}
